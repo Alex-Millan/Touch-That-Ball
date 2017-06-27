6e15aa4fe1f67b148710075d182db14434c4c872
AlexMillan <almillan@ucsc.edu>
2017-06-27 10:37:56 -0700

    Zero gravity balls, they now continuously move rather than slowly building up in the bottom of the screen

---
+++ Unity/TMB/Assets/scrpt/Random.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random : MonoBehaviour {
+    public Rigidbody2D rb;
+    public float thrust;
+	// Use this for initialization
+	void Start () {
+        rb.GetComponent<Rigidbody2D>();
+        Vector2 movement = new Vector2(25000, 25000);
+        rb.AddForce(movement);
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ Unity/TMB/Assets/scrpt/trailRender.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trailRender : MonoBehaviour {
+
+    public TrailRenderer trail;
+    // Use this for initialization
+    void Start()
+    {
+        trail.sortingLayerName = "Default";
+        trail.sortingOrder = 6;
+    }
+    // Update is called once per frame
+    void Update () {
+
+
+
+    }
+}
